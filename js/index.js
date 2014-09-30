205a238f5eab36bb67d6c587e8a9ebac4d8ba00d
salexseen <salexseen@gmail.com>
2014-09-30 23:11:51 +0300

    add options

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@
 
     var world = d.getElementById('world');
     var context, canvas;
+    var options = {
+        w: 10,
+        h: 10,
+        wCount: 1000,
+        hCount: 500
+    };
 
     if (world && world.getContext){
         context = world.getContext('2d');
